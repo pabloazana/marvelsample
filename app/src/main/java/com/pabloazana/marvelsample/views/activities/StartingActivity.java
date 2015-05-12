ff5b9539997240223abf3cd7892bb0e93c0cf5ae
pabloazana <pablo.azana@gmail.com>
2015-05-12 21:07:40 +0200

    Deleting from repository unecesary files

--- app/src/main/java/com/pabloazana/marvelsample/views/activities/StartingActivity.java
+++ app/src/main/java/com/pabloazana/marvelsample/views/activities/StartingActivity.java
@@ -1,12 +1,8 @@
 package com.pabloazana.marvelsample.views.activities;
 
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.pabloazana.marvelsample.views.fragments.FeaturedView;
 import com.pabloazana.marvelsample.views.fragments.NavigationDrawerFragment;
@@ -39,25 +35,4 @@ public class StartingActivity extends BaseActivity implements NavigationDrawerCa
         fragmentManager.beginTransaction().replace(R.id.container, FeaturedView.newInstance()).commit();
     }
 
-    public static class PlaceholderFragment extends Fragment {
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_starting, container, false);
-            return rootView;
-        }
-    }
 }
